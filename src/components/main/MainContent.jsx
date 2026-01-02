c339fec0b2bf600cf8243986566481e946f21912
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-02 01:06:55 -0300

    updated. I adjusted some things and i will work on skills tomorrow

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -1,26 +1,21 @@
 import '../../style/main/mainContent.css'
+import { LuArrowBigDownDash } from "react-icons/lu";
+import '../../style/main/my_img.css'
 const MainContent = () => {
+
+
+  
 
   
     return(
         <>
           <main>
             <section  className='infoContent'>
-              <div>
-                <h3>Sobre mim</h3>
-                <p>Me chamo Vinícius. Tenho 18 anos.</p>
-                <p>Gosto bastante de tecnologia e informática. Comecei a estudar a cerca de 1 ano e meio. Porém só agora eu comecei a praticar em projetos reais.</p>
-
-                <div>
-                  
-                </div>
-                <br/>
+              <div className='text_about_me'>
+                 <h3>Sobre mim  <LuArrowBigDownDash id='arrow_me' /></h3>
+                 <p>Me chamo Vinícius. Tenho 18 anos.</p>
+                 <p>Gosto bastante de tecnologia e informática. Comecei a estudar a cerca de 1 ano e meio. Porém só agora eu comecei a praticar em projetos reais.</p>
               </div>
-
-              <div className='area_photo'>
-
-              </div>
-
             </section>
           </main>
         </>
--- src/components/main/Skills.jsx
+++ src/components/main/Skills.jsx
@@ -11,6 +11,12 @@ const Skills = () => {
     
     return(
         <>
+
+        <div>
+            <div>
+                <h3>Skills</h3>
+            </div>
+        </div>
         
         </>
     )
