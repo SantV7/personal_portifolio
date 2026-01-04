0df178a4b517582ef9b71b2fa3f597e904b13ee4
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-04 00:21:31 -0300

    changed icon on skills, icon js

--- src/components/main/Skills.jsx
+++ src/components/main/Skills.jsx
@@ -1,6 +1,6 @@
 import { FaCss3Alt } from "react-icons/fa";
 import { FaHtml5 } from "react-icons/fa";
-import { FaJsSquare } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
 import { FaGithub } from "react-icons/fa6";7
 import { FaGitAlt } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
@@ -21,7 +21,7 @@ const Skills = () => {
         <div id="area_skills">
             <div><FaHtml5 className="icons_skills" id="icon_html"/></div>
             <div><FaCss3Alt className="icons_skills" id="icon_css"/></div>
-            <div><FaJsSquare className="icons_skills" id="icon_js" /></div>
+            <div><IoLogoJavascript  className="icons_skills" id="icon_js" /></div>
             <div><FaReact className="icons_skills" id="icon_react"/></div>
             <div><FaGitAlt className="icons_skills" id="icon_git"/></div>
             <div id="box-github">
@@ -29,7 +29,6 @@ const Skills = () => {
                 <a href="https://github.com/SantV7"><FaGithub className="icons_skills" id="icon_github"/></a>
             </div>
         </div>
-        
         </>
     )
 }
