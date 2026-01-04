404bbde26e615e70cae5ae1a5261c726f13c07e7
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-04 11:59:59 -0300

    adjust ScrollReveal

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -14,25 +14,29 @@ const MainContent = () => {
 
 
    ScrollReveal().reveal('#skills_container' ,{
-    reset: true ,
-    duration: 1240 ,
-    delay: 200
+    reset: true , 
+    duration: 840 ,
+    delay: 170
    })
 
   
+
+
+
     return(
         <>
           <main>
             <section  className='infoContent'>
-              <div className='text_about_me'>
-                 <h3>Sobre mim  <LuArrowBigDownDash id='arrow_me' /></h3>
-                 <p>Me chamo Vinícius. Tenho 18 anos.</p>
-                 <p>Gosto bastante de tecnologia e informática. Comecei a estudar a cerca de 1 ano e meio. Porém só agora eu comecei a praticar em projetos reais.</p>
-              </div>
+               <div className='text_about_me'>
+                  <h3>Sobre mim  <LuArrowBigDownDash id='arrow_me' /></h3>
+                  <p>Me chamo Vinícius. Tenho 18 anos.</p>
+                  <p>Gosto bastante de tecnologia e informática. Comecei a estudar a cerca de 1 ano e meio.
+                 Porém só agora eu comecei a praticar em projetos reais.</p>
+               </div>
             </section>
 
             <section id='skills_container'>
-              <Skills />
+               <Skills />
             </section>
           </main>
         </>
