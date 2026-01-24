7f0f69b410d5b72cab980712bc5a4b75ae89e63a
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-23 22:45:24 -0300

    Formating text

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -14,15 +14,18 @@ const MainContent = () => {
       ScrollReveal().reveal('.text_about_me' ,{
          reset: true ,
          duration: 1040,
-         delay: 130,
-         origin:'left'
+         delay: 130
       },)
+
+      ScrollReveal().reveal('.paragraph_text_about_me', {
+         reset: true,
+         origin:'left'
+      })
 
       ScrollReveal().reveal('#skills_container' ,{
          reset: true , 
          duration: 1050,
-         delay: 130,
-         origin:'left'
+         delay: 130
       },)
    }, [])
 
@@ -33,10 +36,17 @@ const MainContent = () => {
           <main id='main-info-content'>
             <section className='infoContent'>
                <div id='inicio-text' className='text_about_me'>
-                  <h3>Sobre mim  <LuArrowBigDownDash id='arrow_me' /></h3> <lord-icon id="icon-man-people" src="https://cdn.lordicon.com/daeumrty.json"  colors="primary:#d59f80,secondary:#848484,tertiary:#0a4e5c,quaternary:#242424" delay="1730" state="hover-hower" trigger="loop"></lord-icon>
-                  <p>Me chamo Vinícius. Tenho 18 anos.</p>
-                  <p>Sou um desenvolvedor Frontend Júnior apaixonado por tecnologia, com foco em criar interfaces modernas e funcionais. Minha jornada na programação começou há cerca de um ano e meio, e atualmente dedico-me ao domínio das tecnologias essenciais para a web: <span className="span-usage-language" id='html-span'>HTML</span>, <span className="span-usage-language" id='css-span'>CSS</span>, <span className="span-usage-language" id='javascript-span'>JavaScript</span> e <span className="span-usage-language" id='react-span'>React</span>.</p>
-                  <p>Estou em busca da minha primeira oportunidade profissional na área de tecnologia, onde possa contribuir com minhas habilidades, aprender com uma equipe experiente e evoluir constantemente como desenvolvedor.</p>
+                  <h3>Sobre mim  <LuArrowBigDownDash id='arrow_me' /></h3> 
+                  <lord-icon id="icon-man-people" src="https://cdn.lordicon.com/daeumrty.json"  
+                  colors="primary:#d59f80,secondary:#848484,tertiary:#0a4e5c,quaternary:#242424" 
+                  delay="1730" state="hover-hower" trigger="loop"></lord-icon>
+
+                  <p className='paragraph_text_about_me'>Me chamo Vinícius. Tenho 18 anos.</p>
+                  <p className='paragraph_text_about_me'>Sou um Desenvolvedor Web - Frontend, gosto bastante de tecnologia.
+                   Tenho o foco em criar interfaces modernas e funcionais. Minha jornada na programação começou há cerca de um ano e meio,
+                   e atualmente dedico-me ao domínio das tecnologias essenciais para a web: HTML,CSS, JavaScript e React.</p>
+                  <p className='paragraph_text_about_me'>Estou em busca da minha primeira oportunidade profissional na área de tecnologia, 
+                  onde possa contribuir com minhas habilidades, aprender com uma equipe experiente e evoluir constantemente como desenvolvedor.</p>
                </div>          
             </section>
 
