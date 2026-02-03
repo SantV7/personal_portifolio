7a0c9df48c2fc50e16454879e7e5a3fe815ad135
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-02-03 15:38:54 -0300

    adding irreal photos on card's projects

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import BtnContact from "../navbar/BtnContact"
 const Navbar = () => {
 
    const waitForProjects = () => {
-     alert('Ainda estou preparando essa parte, espere mais um pouco.\nAss by: Vinícius')
+     alert('Espere mais um pouco. As imagens são só ilustrações, e não projetos reais...\nAss by: Vinícius')
    }
 
     return(
@@ -13,7 +13,7 @@ const Navbar = () => {
         <nav id='nav-desktop'>
             <ul id='ul-desktop'>
                 <li><a className='a_navbar' href="#inicio-text">Inicio</a></li>
-                <li><a onClick={() => waitForProjects()} href="#">Projetos</a></li>
+                <li><a onClick={() => waitForProjects()} href="#projects-area">Projetos</a></li>
                 <li><a href="#skills_container">Skills</a></li>
                 <BtnContact />
             </ul>
