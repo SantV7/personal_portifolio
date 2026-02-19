cafacf80eff79c2b0dfc6390f00a92da4af1c038
Vinícius Santos <3izuna@gmail.com>
2026-02-19 20:53:34 -0300

    puting scrollTriggger

--- src/components/footer/Services.jsx
+++ src/components/footer/Services.jsx
@@ -7,23 +7,52 @@ import '../../style/footer/services.css'
 import { MdEdgesensorHigh } from "react-icons/md";
 import ScrollReveal from "scrollreveal";
 import {useEffect} from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Services = () => {  
    useEffect(() => {
-
      ScrollReveal().reveal('#footer', {
        reset: true ,
        duration:2,
        opacity: 1
      })
+
+
+     gsap.fromTo('.icones_contact_link', {
+      x: -20,
+      opacity: 0
+     }, {
+      opacity: 1,
+      duration: 1,
+      ease: 'power2',
+       scrollTrigger: {
+          trigger: '#area_icons_contact',
+          start: 'top 90%', 
+          end: 'bottom 60%',
+          toggleActions: 'play none none reverse',
+          },
+      stagger: {
+        each: 0.2,
+        from: 'start'
+      },
+      x: 0      
+     })
    }, [])
+
+
+
+
+
 
     return(
         <>
           <footer id="footer">
             <div id="services_info">
                 <p className="text_footer">Para serviços</p>
-                <span className="text_footer">Entre em contato <MdEdgesensorHigh size={30} color="orange" /> </span>
+                <span className="text_footer">Entre em contato <MdEdgesensorHigh size={30} color="orange" /></span>
             </div>
 
 
