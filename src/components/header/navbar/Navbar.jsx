e7df667a4b86538d2caadc17b6084280f7391b22
Vinícius Santos <3izuna@gmail.com>
2026-02-19 21:24:08 -0300

    today is it

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -2,10 +2,23 @@ import { PiHouseLineDuotone } from "react-icons/pi";
 import BtnContact from "../navbar/BtnContact"
 import { GrProjects } from "react-icons/gr";
 import { AiOutlineCluster } from "react-icons/ai";
+import { useEffect } from "react";
+import gsap from "gsap";
 
 
 const Navbar = () => {
-
+    useEffect(() => {
+        gsap.fromTo('.li-gsap', {
+            opacity: 0.65,
+            scale: 0.95,
+            y: -140
+        }, {
+            y: 0, 
+            duration: 1.8,
+            ease: 'power2',
+            opacity: 1
+        })
+    }, [])
    const waitForProjects = () => {
      alert('Espere mais um pouco. As imagens são só ilustrações, e não projetos reais...\nAss by: Vinícius')
    }
@@ -15,9 +28,9 @@ const Navbar = () => {
 
         <nav id='nav-desktop'>
             <ul id='ul-desktop'>
-                <li><a className='a_navbar' href="#inicio-text">Inicio</a> <div><PiHouseLineDuotone className="icon-header-hover" size={27} /></div></li>
-                <li><a onClick={() => waitForProjects()} href="#projects-area">Projetos</a> <div><GrProjects size={21.65} className="icon-header-hover" /></div></li>
-                <li><a href="#skills_container">Skills</a> <AiOutlineCluster className="icon-header-hover" size={30} /></li>
+                <li className="li-gsap"><a className='a_navbar' href="#inicio-text">Inicio</a> <div><PiHouseLineDuotone className="icon-header-hover" size={27} /></div></li>
+                <li className="li-gsap"><a onClick={() => waitForProjects()} href="#projects-area">Projetos</a> <div><GrProjects size={21.65} className="icon-header-hover" /></div></li>
+                <li className="li-gsap"><a href="#skills_container">Skills</a> <AiOutlineCluster className="icon-header-hover" size={30} /></li>
                 <BtnContact />
             </ul>
         </nav>
